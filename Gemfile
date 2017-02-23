a296ce1cfff10de92e6ad71ac3b6081814de3c40
Edouard Roussillon <edouardroussillon@concrete.com.br>
2017-02-23 08:41:49 -0300

    setup poject hero

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source "https://rubygems.org"
+
+#CircleCI
+#gem 'minitest-ci', :git => 'git@github.com:circleci/minitest-ci.git'
+
+gem 'cocoapods', '1.1.1'
---
+++ Pods/Target Support Files/Pods-TestHeroUITests/Pods-TestHeroUITests-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_TestHeroUITestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_TestHeroUITestsVersionString[];
+
